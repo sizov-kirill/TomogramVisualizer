6fa4e3a4b66406dbb943950180328c3b07e0047f
sizov1 <sizow.k.d@gmail.com>
2020-04-14 12:30:45 +0300

    tomogram visualizer version 1

---
+++ TomogramVisualizer3/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace TomogrammVisualizer3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.glControl1 = new OpenTK.GLControl();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // glControl1
+            // 
+            this.glControl1.BackColor = System.Drawing.Color.Black;
+            this.glControl1.Location = new System.Drawing.Point(232, 173);
+            this.glControl1.Name = "glControl1";
+            this.glControl1.Size = new System.Drawing.Size(750, 600);
+            this.glControl1.TabIndex = 0;
+            this.glControl1.VSync = false;
+            this.glControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.glControl1_Paint);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(7, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(1203, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(96, 28);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(919, 45);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Value = 10;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Номер слоя";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(96, 65);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(919, 45);
+            this.trackBar2.TabIndex = 4;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(96, 102);
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(919, 45);
+            this.trackBar3.TabIndex = 5;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Минимум TF";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Ширина TF";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(1040, 28);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(151, 100);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Способ визуализации";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(7, 43);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(68, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Texture";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(7, 20);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(61, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Quads";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1203, 785);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.glControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Визуализация томограммы";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private OpenTK.GLControl glControl1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ TomogramVisualizer3/Form1.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace TomogrammVisualizer3
+{
+    public partial class Form1 : Form
+    {
+        Bin bin = new Bin();
+        View view = new View();
+        bool loaded = false; // переменная сигнализирующая о процессе загрузки данные
+                             // false данные не загружены
+                             // true данные загружены
+        int currentLayer = 0;
+        bool needReload = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string str = dialog.FileName;
+                bin.readBIN(str);
+                view.SetupView(glControl1.Width, glControl1.Height);
+                trackBar1.Maximum = Bin.Z - 1;
+                loaded = true; // сигнализируем о том, что данные загружены
+                glControl1.Invalidate();
+            }
+
+        }
+
+        private void glControl1_Paint(object sender, PaintEventArgs e)
+        {
+            if (loaded)
+            {
+                // отрисовываем только если данные загружены 
+                if (radioButton1.Checked)
+                    // режим отрисовки прямоугольниками
+                    view.DrawQuads(currentLayer);
+                else
+                {
+                    // режим рисования текстурой 
+                    if (needReload) // проверяем надобность загрузки новой текстуры в видеопамять
+                    {
+                        view.generateTextureImage(currentLayer);
+                        view.Load2DTexture();
+                        needReload = false;
+                    }
+                    view.DrawTexture();
+                }
+                glControl1.SwapBuffers();
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            // trackBar изменяющий номер слоя томограммы 
+            currentLayer = trackBar1.Value;
+            needReload = true;
+        }
+
+        void Application_Idle(object sender, EventArgs e)
+        {
+            while (glControl1.IsIdle)
+            {
+                // пока glControl свободен выполняем рендеринг
+                displayFPS();
+                glControl1.Invalidate();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Application.Idle += Application_Idle;
+        }
+
+        int FrameCount;
+        DateTime NextFPSUpdate = DateTime.Now.AddSeconds(1);
+        void displayFPS()
+        {
+            // функция просчитывает число отрендернных кадров в секунду (fps)
+            // и выводит fps на экран
+            if (DateTime.Now >= NextFPSUpdate)
+            {
+                this.Text = String.Format("CT Visualizer (fps={0})", FrameCount);
+                NextFPSUpdate = DateTime.Now.AddSeconds(1);
+                FrameCount = 0;
+            }
+            FrameCount++;
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            // trackBar изменяющий значение числа min для функции TF 
+            view.minimum = trackBar2.Value;
+            needReload = true; // изменение TF требует загрузки новой текстуры в видеопамять
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            // trackBar изменяющий ширину окан для функции TF 
+            view.windowWidth = trackBar3.Value;
+            needReload = true; // изменение TF требует загрузки новой текстуры в видеопамять 
+        }
+
+        class Bin
+        {
+            public static int X, Y, Z;
+            public static short[] array;
+            public Bin() { }
+
+
+
+            public void readBIN(string path)
+            {
+                // функция считывания бинарного файла
+                if (File.Exists(path))
+                {
+                    BinaryReader reader =
+                        new BinaryReader(File.Open(path, FileMode.Open));
+
+                    X = reader.ReadInt32();
+                    Y = reader.ReadInt32();
+                    Z = reader.ReadInt32();
+
+                    int arraySize = X * Y * Z;
+                    array = new short[arraySize];
+                    for (int i = 0; i < arraySize; ++i)
+                    {
+                        array[i] = reader.ReadInt16();
+                    }
+
+                }
+            }
+        }
+
+        class View
+        {
+            public int minimum = 0;
+            public int windowWidth = 2000;  // шириина окна TF
+
+            public void SetupView(int width, int height)
+            {
+                GL.ShadeModel(ShadingModel.Smooth);
+                GL.MatrixMode(MatrixMode.Projection);
+                GL.LoadIdentity(); // заменяем текущую матрицу единичной матрицей
+                GL.Ortho(0, Bin.X, 0, Bin.Y, -1, 1);
+                GL.Viewport(0, 0, width, height);
+            }
+
+            int clamp(int value, int min, int max)
+            {
+                return Math.Min(max, Math.Max(min, value));
+            }
+
+            Color TransferFunction(short value)
+            {
+                int min = minimum;
+                int max = minimum + windowWidth;
+                int newVal = clamp((value - min) * 255 / (max - min), 0, 255);
+                return Color.FromArgb(255, newVal, newVal, newVal);
+            }
+
+            public void DrawQuads(int layerNumber)
+            {
+                short value;
+                GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+                for (int x_coord = 0; x_coord < Bin.X - 1; x_coord++)
+                {
+                    // после первых двух вершин, каждая последующая пара вершин будет добавлять один четырёхугольник
+                    GL.Begin(BeginMode.QuadStrip);
+
+                    // отрисовываем первые две вершины
+                    value = Bin.array[x_coord + layerNumber * Bin.X * Bin.Y];
+                    GL.Color3(TransferFunction(value));
+                    GL.Vertex2(x_coord, 0);
+
+                    value = Bin.array[(x_coord + 1) + layerNumber * Bin.X * Bin.Y];
+                    GL.Color3(TransferFunction(value));
+                    GL.Vertex2(x_coord + 1, 0);
+
+                    for (int y_coord = 0; y_coord < Bin.Y - 1; y_coord++)
+                    {
+                        // отрисовываем ещё две вершины 
+
+                        value = Bin.array[x_coord + y_coord * Bin.X
+                            + layerNumber * Bin.X * Bin.Y];
+                        GL.Color3(TransferFunction(value));
+                        GL.Vertex2(x_coord, y_coord);
+
+                        value = Bin.array[(x_coord + 1) + y_coord * Bin.X
+                            + layerNumber * Bin.X * Bin.Y];
+                        GL.Color3(TransferFunction(value));
+                        GL.Vertex2(x_coord + 1, y_coord);
+                    }
+                    GL.End();
+                }
+                
+            }
+
+
+            Bitmap textureImage;
+            int VBOtexture;
+            public void Load2DTexture()
+            {
+                // функция загрузки текстуры в видеопамять
+
+                // выбираем указанную текстуру VBOtexture как активную
+                GL.BindTexture(TextureTarget.Texture2D, VBOtexture);
+
+
+                // блокируем textureImage в оперативной памяти только на чтение
+                // в data хранятся сведения об операции блокировки
+                BitmapData data = textureImage.LockBits(
+                    new System.Drawing.Rectangle(0, 0, textureImage.Width, textureImage.Height),
+                    ImageLockMode.ReadOnly,
+                    System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+                // текстурируем изображение в активную текстуру для того, 
+                // чтобы дать шейдерам возможность считывать его элементы
+                // data.Scan0 - адресс первого пикселя 
+                GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba,
+                    data.Width, data.Height, 0, OpenTK.Graphics.OpenGL.PixelFormat.Bgra,
+                    PixelType.UnsignedByte, data.Scan0);
+
+                // разблокировываем textureImage
+                textureImage.UnlockBits(data);
+
+                // присваиваем значения (int)TextureMinFilter.Linear параметру текстуры
+                // тем самым определяем как будет происходить процесс минимизации текстуры
+                // TextureMinFilter.Linear соответвует тому, что значения при мимнизации
+                // вычисляются как среднее арифметическое взвешенное четырех элементов
+                // текстуры, которые ближе всего к заданному элементу текстуры 
+                GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter,
+                    (int)TextureMinFilter.Linear);
+
+                // присваиваем значения (int)TextureMagFilter.Linear параметру текстуры
+                // тем самым определяем как будет происходить процесс увеличения текстуры
+                // TextureMagFilter.Linear соответвует тому же, что и для TextureMinFilter.Linear
+                GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter,
+                    (int)TextureMagFilter.Linear);
+
+                ErrorCode Er = GL.GetError();
+                string str = Er.ToString();
+            }
+
+            public void generateTextureImage(int layerNumber)
+            {
+                // функция преобразует томограмму в изображение 
+                textureImage = new Bitmap(Bin.X, Bin.Y);
+                for (int i = 0; i < Bin.X; ++i)
+                    for (int j = 0; j < Bin.Y; ++j)
+                    {
+                        // получаем номер пикселя (i, j) в исходдном массиве данных 
+                        int pixelNumber = i + j * Bin.X + layerNumber * Bin.X * Bin.Y;
+
+                        // преобразовываем значени плотности в цвет и устанавливаем этот цвет в текущий пиксель
+                        textureImage.SetPixel(i, j, TransferFunction(Bin.array[pixelNumber]));
+                    }
+            }
+
+            public void DrawTexture()
+            {
+                GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+                // включаем 2D текстурирование
+                GL.Enable(EnableCap.Texture2D);
+
+                // выбираем указанную текстуру VBOtexture как активную
+                GL.BindTexture(TextureTarget.Texture2D, VBOtexture);
+
+                // рисуем один прямоугольник с наложенной текстурой 
+                GL.Begin(BeginMode.Quads);
+                GL.Color3(Color.White);
+
+                // задаем 4 угла прямоугольника и накладываем на него текстуру
+                GL.TexCoord2(0f, 0f);
+                GL.Vertex2(0, 0);
+                GL.TexCoord2(0f, 1f);
+                GL.Vertex2(0, Bin.Y);
+                GL.TexCoord2(1f, 1f);
+                GL.Vertex2(Bin.X, Bin.Y);
+                GL.TexCoord2(1f, 0f);
+                GL.Vertex2(Bin.X, 0);
+
+                GL.End();
+
+                // выключаем 2D текстурирование
+                GL.Disable(EnableCap.Texture2D);
+            }
+        }
+
+    }
+}
+
